e03364b6b54b34143d970aca7696f0a316417d31
esaki <esaki@esakinoMacBook-Air.local>
2026-06-23 22:09:49 +0900

    set debug false

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9-6qg&rxtdk9!#$mp6oqn+m9-wk)uywrw6jw53zur7jnb+a7qi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
