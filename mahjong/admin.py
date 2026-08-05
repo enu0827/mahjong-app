716421dc2d61e3319b4708e222f46496f1ffd7d5
esaki <esaki@d058-033.p058.chem.sci.osaka-u.ac.jp>
2026-08-05 19:07:23 +0900

    Add carryover stats to Player

--- mahjong/admin.py
+++ mahjong/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Player, Season, Game, Result
 
-admin.site.register(Player)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "carryover_profit",
+        "carryover_game_count",
+        "carryover_first_count",
+        "carryover_second_count",
+        "carryover_third_count",
+        "carryover_fourth_count",
+    )
+
+
 admin.site.register(Season)
 admin.site.register(Game)
 admin.site.register(Result)
-
 # Register your models here.
--- mahjong/models.py
+++ mahjong/models.py
@@ -1,17 +1,45 @@
 from django.db import models
 
-
 class Player(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+    carryover_profit = models.FloatField(
+        default=0,
+        verbose_name="第7節までの持ち越し収支"
+    )
+
+    carryover_game_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name="第7節までの対局数"
+    )
+
+    carryover_first_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name="第7節までの1着回数"
+    )
+
+    carryover_second_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name="第7節までの2着回数"
+    )
+
+    carryover_third_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name="第7節までの3着回数"
+    )
+
+    carryover_fourth_count = models.PositiveIntegerField(
+        default=0,
+        verbose_name="第7節までの4着回数"
+    )
 
     def __str__(self):
         return self.name
 
-
 class Season(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateField()
-    end_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
