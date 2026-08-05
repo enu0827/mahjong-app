93c3aa4b02bfab1c9febcb72399574305ccd5307
esaki <esaki@d058-033.p058.chem.sci.osaka-u.ac.jp>
2026-08-05 18:10:30 +0900

    temporarily enable debug

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
